78c70811bd36f8b65315525582664d4e7ddd27d6
raktim <raktimmitra14@gmail.com>
2023-09-05 16:26:48 +0530

    Update conn_msaccess.py
    Code to connect to msaccess database

--- conn_msaccess.py
+++ conn_msaccess.py
@@ -9,7 +9,7 @@ st.code(f'MS-Access Drivers : {msa_drivers}')
 
 
 con_string = r'DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};' \
-                r'DBQ=C:\Users\315604\Desktop\sql_generator\pydb1.accdb;'
+                r'DBQ=C:\Users\315604\Desktop\sql_generator\pydb1.accdb;' #add your's databse file location
 conn = pyodbc.connect(con_string)
 
 cur = conn.cursor()
@@ -18,7 +18,3 @@ cur.execute('SELECT * FROM emp')
 for row in cur.fetchall():
     st.code(row)
 
-
-
-# except pyodbc.Error as e:
-#     print("Error in Connection")
